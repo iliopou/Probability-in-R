219b06bb3647da35bda9f4f6c59b8633bd9cad57
Vasileios Iliopoulos <109439587+iliopou@users.noreply.github.com>
2022-07-26 12:17:22 +0200

    Update insertion_sort.r
    Added isTRUE () function.

--- insertion_sort.r
+++ insertion_sort.r
@@ -2,7 +2,7 @@ insertion_sort <- function(A) {if (length(A) > 1) {
   for (j in 2:length(A)) {
     key  <- A[j] 
     i <- j - 1 
-    while (i > 0 && A[i] > key) {             
+    while (isTRUE(i > 0 && A[i] > key)) {             
       A[(i + 1)] <- A[i]
       i <- i - 1 
     }
