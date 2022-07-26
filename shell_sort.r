ae6d14165de8a6a71f0d500337e77da489adac59
Vasileios Iliopoulos <109439587+iliopou@users.noreply.github.com>
2022-07-26 12:13:51 +0200

    Update shell_sort.r

--- shell_sort.r
+++ shell_sort.r
@@ -1,4 +1,4 @@
-shell_sort <- function(A){
+shell_sort <- function(A) { if (length(A) > 1 ) {
 gaps <- c(rev((2^seq(0, floor(log(length(A)-1,2)), 1)))) ;       # Vector of increment sequence of powers of 2
   
  for (i in gaps) {
@@ -7,14 +7,14 @@ gaps <- c(rev((2^seq(0, floor(log(length(A)-1,2)), 1)))) ;       # Vector of inc
       key <- A[el];
 	    j <- el;
 	   
-  while (j > i && A[j-i] > key) {                               
+  while (isTRUE(j > i && A[j-i] > key)) {                               
 	 A[j] <- A[j-i];
 	 j <- j-i;
 	 }
 	 A[j] <- key 
 	 }
   }                                                            # Last iteration is insertion sort 1 position apart
-
+ }
 return (A)                                                     # In place sorting
 }                                                              
   
